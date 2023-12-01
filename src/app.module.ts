1f4a1402efdd65af50f4f3d459d36b68419dff3a
Seung-wan <tmddhks0104@gmail.com>
2023-12-01 22:53:06 +0900

    chore: prisma를 세팅합니다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from '@/prisma.service';
+
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+
+import { PostService } from '@/post.service';
+
+import { UserService } from '@/user.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
